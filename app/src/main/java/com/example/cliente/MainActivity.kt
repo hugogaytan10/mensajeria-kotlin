9fa35d3c3dd5070578dcc1ae7ca695476e1ea3ce
Clementain <clentecarmona@gmail.com>
2023-04-30 19:35:48 -0500

    Se supone envia el token al servidor pero se presentan varios problemas: 1.- posiblemente el firewall 2.- el servidor debemos de hacer que espere un momento, porque parece que recibe un token "fantasma", hay que ponerle un "espera"o algo, en el siguiente commit se adjunta el servidor

--- app/src/main/java/com/example/cliente/MainActivity.kt
+++ app/src/main/java/com/example/cliente/MainActivity.kt
@@ -1,50 +1,28 @@
 package com.example.cliente
 
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import com.google.firebase.messaging.FirebaseMessaging
 
 
 class MainActivity : AppCompatActivity() {
-    // Declare the launcher at the top of your Activity/Fragment:
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // FCM SDK (and your app) can post notifications.
-        } else {
-            // TODO: Inform user that that your app will not show notifications.
-        }
-    }
-
-    private fun askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    this, android.Manifest.permission.POST_NOTIFICATIONS
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                // FCM SDK (and your app) can post notifications.
-            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
-            } else {
-                // Directly ask for the permission
-                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
+    companion object {
+        private const val TAG = "MainActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        askNotificationPermission()
-
+        // Suscribirse a un tema de Firebase Messaging
+        FirebaseMessaging.getInstance().subscribeToTopic("topic_name")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Suscrito al tema.")
+                } else {
+                    Log.e(TAG, "Error al suscribirse al tema.", task.exception)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/cliente/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/cliente/MyFirebaseMessagingService.kt
@@ -1,29 +1,56 @@
 package com.example.cliente
 
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
-    val TAG = "MiToken"
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-        Log.d(TAG, "Mensaje en primer: ${message.notification!!.title}")
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Nuevo token: $token")
+        enviarTokenAServidor(token)
     }
 
-    /**
-     * Called if the FCM registration token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the
-     * FCM registration token is initially generated so this is where you would retrieve the token.
-     */
-    override fun onNewToken(token: String) {
-        Log.d(TAG, "Refreshed token: $token")
+    private fun enviarTokenAServidor(token: String) {
+        // Llamar a la ruta "/registerToken" en tu servidor para registrar el token
+        // Utiliza una biblioteca de cliente HTTP como OkHttp o Retrofit para hacer la solicitud
+        // ...
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
-        //sendRegistrationToServer(token)
+        // Ejemplo utilizando la biblioteca OkHttp
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://192.168.0.5:3000/registerToken")
+            .post("{\"token\":\"$token\"}".toRequestBody("application/json".toMediaTypeOrNull()))
+            .build()
+
+        client.newCall(request).execute()
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "Mensaje recibido de: ${remoteMessage.from}")
+
+        // Crear una notificación para el mensaje recibido
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(remoteMessage.notification?.title)
+            .setContentText(remoteMessage.notification?.body)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+
+        // Mostrar la notificación
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMessagingService"
+        private const val CHANNEL_ID = "my_channel_id"
     }
 }
