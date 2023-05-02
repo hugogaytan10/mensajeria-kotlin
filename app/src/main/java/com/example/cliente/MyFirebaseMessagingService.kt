3d9d68981494727961dcd631004a6f0ea5ee92a1
Clementain <clentecarmona@gmail.com>
2023-05-02 01:48:50 -0500

    Primeras pruebas de la api

--- app/src/main/java/com/example/cliente/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/cliente/MyFirebaseMessagingService.kt
@@ -1,56 +1,96 @@
 package com.example.cliente
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.util.Log
+import android.media.RingtoneManager
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
-
+import org.json.JSONObject
+import java.io.IOException
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
-        Log.d(TAG, "Nuevo token: $token")
-        enviarTokenAServidor(token)
-    }
-
-    private fun enviarTokenAServidor(token: String) {
-        // Llamar a la ruta "/registerToken" en tu servidor para registrar el token
-        // Utiliza una biblioteca de cliente HTTP como OkHttp o Retrofit para hacer la solicitud
-        // ...
-
-        // Ejemplo utilizando la biblioteca OkHttp
-        val client = OkHttpClient()
-        val request = Request.Builder()
-            .url("http://192.168.0.5:3000/registerToken")
-            .post("{\"token\":\"$token\"}".toRequestBody("application/json".toMediaTypeOrNull()))
-            .build()
-
-        client.newCall(request).execute()
+        // Aquí se debe enviar el token al servidor
+        //sendNotification(token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d(TAG, "Mensaje recibido de: ${remoteMessage.from}")
-
-        // Crear una notificación para el mensaje recibido
-        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(remoteMessage.notification?.title)
-            .setContentText(remoteMessage.notification?.body)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-
-        // Mostrar la notificación
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
+        // Aquí se debe manejar la recepción del mensaje
+        // y mostrar una notificación en la aplicación
+        remoteMessage.notification?.let { notification ->
+            val title = notification.title ?: getString(R.string.app_name)
+            val body = notification.body ?: ""
+            showNotification(title, body)
+        }
     }
 
+    private fun sendNotification(token: String, title: String, body: String, data: Map<String, String>) {
+        val url = "https://fcm.googleapis.com/v1/projects/<PROJECT_ID>/messages:send"
+
+        val client = OkHttpClient()
+
+        val json = JSONObject().apply {
+            put("message", JSONObject().apply {
+                put("token", token)
+                put("notification", JSONObject().apply {
+                    put("title", title)
+                    put("body", body)
+                })
+                put("data", JSONObject(data))
+            })
+        }
+
+        val requestBody = json.toString().toRequestBody("application/json".toMediaType())
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .addHeader("Authorization", "Bearer <YOUR_SERVER_KEY>")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(TAG, "Error al enviar la notificación", e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d(TAG, "Notificación enviada correctamente")
+            }
+        })
+    }
+
+
+    private fun showNotification(title: String, body: String) {
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "CanalNotis"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, channelName, importance)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
     companion object {
-        private const val TAG = "MyFirebaseMessagingService"
-        private const val CHANNEL_ID = "my_channel_id"
+        private const val TAG = "MyFirebaseMessagingSvc"
     }
 }
